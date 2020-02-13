e4005fe564c7ced4d25adbb822dd1588dc4d0057
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-13 14:28:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map(source_array)
   new_array = []
   counter = 0
   while counter < source_array.length do
-  new_array << yield(array[counter])
+  new_array << yield(source_array[counter])
   counter += 1
   end
 new_array
